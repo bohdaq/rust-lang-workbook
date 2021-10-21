c5d2bdc38ea975145a2405c5ae8dc986a5eacffd
Bohdan Tsap <bohdaq@gmail.com>
2021-10-22 00:15:53 +0300

    References and Borrowing

---
+++ chapter_four/references-borrowing/src/main.rs
@@ -0,0 +1,81 @@
+// References and Borrowing
+}    let s1 = String::from("hello");
+
+    let (s2, len) = calculate_length(s1);
+
+    println!("The length of '{}' is {}.", s2, len);
+}
+
+fn calculate_length(s: String) -> (String, usize) {
+    let length = s.len(); // len() returns the length of a String
+
+    (s, length)
+}
+
+//The issue with the tuple code is that we have to return the String to the calling function so we can still use the String after the call to calculate_length, because the String was moved into calculate_length.
+//
+//Here is how you would define and use a calculate_length function that has a reference to an object as a parameter instead of taking ownership of the value:
+
+fn main() {
+    let s1 = String::from("hello");
+
+    let len = calculate_length(&s1);
+
+    println!("The length of '{}' is {}.", s1, len);
+}
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
+}
+
+//First, notice that all the tuple code in the variable declaration and the function return value is gone. Second, note that we pass &s1 into calculate_length and, in its definition, we take &String rather than String.
+//
+//These ampersands are references, and they allow you to refer to some value without taking ownership of it.
+//Note: The opposite of referencing by using & is dereferencing, which is accomplished with the dereference operator, *.
+//
+//Let’s take a closer look at the function call here:
+
+let s1 = String::from("hello");
+let len = calculate_length(&s1);
+
+//The &s1 syntax lets us create a reference that refers to the value of s1 but does not own it. Because it does not own it, the value it points to will not be dropped when the reference stops being used.
+//Likewise, the signature of the function uses & to indicate that the type of the parameter s is a reference. Let’s add some explanatory annotations:
+
+fn calculate_length(s: &String) -> usize { // s is a reference to a String
+    s.len()
+} // Here, s goes out of scope. But because it does not have ownership of what
+  // it refers to, nothing happens.
+
+//The scope in which the variable s is valid is the same as any function parameter’s scope, but we don’t drop what the reference points to when s stops being used because we don’t have ownership. When functions have references as parameters instead of the actual values, we won’t need to return the values in order to give back ownership, because we never had ownership.
+
+//We call the action of creating a reference borrowing. As in real life, if a person owns something, you can borrow it from them. When you’re done, you have to give it back.
+
+//So what happens if we try to modify something we’re borrowing? Spoiler alert: it doesn’t work!
+
+fn main() {
+    let s = String::from("hello");
+
+    change(&s);
+}
+
+fn change(some_string: &String) {
+    some_string.push_str(", world");
+}
+
+//cannot borrow `*some_string` as mutable, as it is behind a `&` reference --> src/main.rs:8:5
+//Just as variables are immutable by default, so are references. We’re not allowed to modify something we have a reference to.
+
+
+//Mutable References
+//We can fix the error in the code with just a few small tweaks:
+fn main() {
+    let mut s = String::from("hello");
+
+    change(&mut s);
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
+}
+
+
