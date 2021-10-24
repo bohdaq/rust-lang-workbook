b221b7d71220772b52f2e73b960478419c44bbfd
Bohdan Tsap <bohdaq@gmail.com>
2021-10-25 01:12:29 +0300

    refs

--- chapter_four/references-borrowing/src/main.rs
+++ chapter_four/references-borrowing/src/main.rs
@@ -78,4 +78,61 @@ fn change(some_string: &mut String) {
     some_string.push_str(", world");
 }
 
+//First, we had to change s to be mut. Then we had to create a mutable reference with &mut s where we call the change function, and update the function signature to accept a mutable reference with some_string: &mut String. This makes it very clear that the change function will mutate the value it borrows.
+//But mutable references have one big restriction: you can have only one mutable reference to a particular piece of data at a time. This code will fail:
+let mut s = String::from("hello");
+
+let r1 = &mut s;
+let r2 = &mut s;
+
+println!("{}, {}", r1, r2);
+
+//error[E0499]: cannot borrow `s` as mutable more than once at a time --> src/main.rs:5:14
+
+//This error says that this code is invalid because we cannot borrow s as mutable more than once at a time. The first mutable borrow is in r1 and must last until it’s used in the println!, but between the creation of that mutable reference and its usage, we tried to create another mutable reference in r2 that borrows the same data as r1.
+//The restriction preventing multiple mutable references to the same data at the same time allows for mutation but in a very controlled fashion. It’s something that new Rustaceans struggle with, because most languages let you mutate whenever you’d like.
+//The benefit of having this restriction is that Rust can prevent data races at compile time. A data race is similar to a race condition and happens when these three behaviors occur:
+//   Two or more pointers access the same data at the same time.
+//   At least one of the pointers is being used to write to the data.
+//   There’s no mechanism being used to synchronize access to the data.
+
+//Data races cause undefined behavior and can be difficult to diagnose and fix when you’re trying to track them down at runtime; Rust prevents this problem from happening because it won’t even compile code with data races!
+//As always, we can use curly brackets to create a new scope, allowing for multiple mutable references, just not simultaneous ones:
+fn main() {
+    let mut s = String::from("hello");
+
+    {
+        let r1 = &mut s;
+    } // r1 goes out of scope here, so we can make a new reference with no problems.
+
+    let r2 = &mut s;
+}
+
+//A similar rule exists for combining mutable and immutable references. This code results in an error:
+fn main() {
+    let mut s = String::from("hello");
+
+    let r1 = &s; // no problem
+    let r2 = &s; // no problem
+    let r3 = &mut s; // BIG PROBLEM
+
+    println!("{}, {}, and {}", r1, r2, r3);
+}
+
+//error[E0502]: cannot borrow `s` as mutable because it is also borrowed as immutable
+//Whew! We also cannot have a mutable reference while we have an immutable one. Users of an immutable reference don’t expect the values to suddenly change out from under them! However, multiple immutable references are okay because no one who is just reading the data has the ability to affect anyone else’s reading of the data.
+
+//Note that a reference’s scope starts from where it is introduced and continues through the last time that reference is used. For instance, this code will compile because the last usage of the immutable references, the println!, occurs before the mutable reference is introduced:fn main() {
+    let mut s = String::from("hello");
+
+    let r1 = &s; // no problem
+    let r2 = &s; // no problem
+    println!("{} and {}", r1, r2);
+    // variables r1 and r2 will not be used after this point
+
+    let r3 = &mut s; // no problem
+    println!("{}", r3);
+}
+
+
 
